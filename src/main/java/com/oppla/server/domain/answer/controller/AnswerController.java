6e7e5296d9a64dbc7183d8afd82fbf528698297c
junbig <kjgi73k@gmail.com>
2023-04-04 21:17:35 +0900

    feat : Swagger 어노테이션 추가 - AnswerController에 Swagger 어노테이션 추가 - AnswerPostReqDto Swagger 어노테이션 추가

--- src/main/java/com/oppla/server/domain/answer/controller/AnswerController.java
+++ src/main/java/com/oppla/server/domain/answer/controller/AnswerController.java
@@ -4,6 +4,7 @@ import com.oppla.server.domain.answer.dto.AnswerPostReqDto;
 import com.oppla.server.domain.answer.service.AnswerService;
 import com.oppla.server.domain.member.entity.Member;
 import com.oppla.server.global.common.response.BaseResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,10 @@ import org.springframework.web.bind.annotation.*;
 public class AnswerController {
     private final AnswerService answerService;
 
+    @Operation(
+            summary = "답변 등록",
+            description = "답변 등록 API"
+    )
     @PostMapping("")
     public BaseResponse postAnswer(@AuthenticationPrincipal Member member, @RequestBody AnswerPostReqDto dto){
         answerService.postAnswer(member, dto);
--- src/main/java/com/oppla/server/domain/answer/dto/AnswerPostReqDto.java
+++ src/main/java/com/oppla/server/domain/answer/dto/AnswerPostReqDto.java
@@ -1,5 +1,6 @@
 package com.oppla.server.domain.answer.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,8 +9,10 @@ import javax.validation.constraints.NotNull;
 @Data
 @NoArgsConstructor
 public class AnswerPostReqDto {
+    @Schema(description = "답변이 달릴 질문의 Id")
     @NotNull
     private Long questionId;
+    @Schema(description = "답변 내용")
     @NotNull
     private String content;
 }
