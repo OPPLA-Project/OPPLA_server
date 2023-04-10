5d63d24f7b0e356791984f31ce3561599ca776af
junbig <kjgi73k@gmail.com>
2023-04-10 21:58:35 +0900

    OPPLA-16 feat : AnswerRepository 메소드 추가 및 AnswerImgRepository 추가
    - AnswerRepository에 다음 메소드 추가
       - findAllByQuestionId();
       - countByMemberId();
       - countByMemberIdAnsSelection();
    
    - AnswerImgRepository.java 생성
       - findAllByAnswerId();

--- src/main/java/com/oppla/server/domain/answer/repository/AnswerRepository.java
+++ src/main/java/com/oppla/server/domain/answer/repository/AnswerRepository.java
@@ -3,5 +3,11 @@ package com.oppla.server.domain.answer.repository;
 import com.oppla.server.domain.answer.entity.Answer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+    List<Answer> findAllByQuestionId(Long questionId);
+    Integer countByMemberId(Long id);
+
+    Integer countByMemberIdAndSelection(Long id, boolean b);
 }
