c988d1394a66dff4ae7b7c8fb8e4d12422d54458
junker <kjgi73k@gmail.com>
2023-05-02 19:42:23 +0900

    feat : Pagination 적용으로 인한 Repository 수정

--- src/main/java/com/oppla/server/domain/answer/repository/AnswerRepository.java
+++ src/main/java/com/oppla/server/domain/answer/repository/AnswerRepository.java
@@ -1,12 +1,12 @@
 package com.oppla.server.domain.answer.repository;
 
 import com.oppla.server.domain.answer.entity.Answer;
+import com.oppla.server.domain.answer.repository.impl.AnswerRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface AnswerRepository extends JpaRepository<Answer, Long> {
-    List<Answer> findAllByQuestionId(Long questionId);
+public interface AnswerRepository extends JpaRepository<Answer, Long>, AnswerRepositoryCustom {
     Integer countByMemberId(Long id);
 
     Integer countByMemberIdAndSelection(Long id, boolean b);
