b244e4cbf9597344abe931a828d36177b0075a4d
roomie-00 <81614420+roomie-00@users.noreply.github.com>
2023-05-12 14:40:30 +0900

    fix: 스웨거 추가

--- src/main/java/com/oppla/server/domain/auth/controller/AuthController.java
+++ src/main/java/com/oppla/server/domain/auth/controller/AuthController.java
@@ -3,6 +3,9 @@ package com.oppla.server.domain.auth.controller;
 import com.oppla.server.domain.auth.dto.AuthReqDto;
 import com.oppla.server.domain.auth.service.AuthService;
 import com.oppla.server.global.common.response.BaseDataResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +15,10 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(summary = "카카오 로그인", description = "카카오 계정으로 로그인 및 회원가입")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "소셜 로그인 성공")
+    })
     @ResponseBody
     @RequestMapping("/kakao")
     public BaseDataResponse<AuthReqDto> kakaoAuth(@RequestBody AuthReqDto authReqDto) {
